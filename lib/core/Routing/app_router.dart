699c9fa975c6a61af1ea7d42b4c14aa2332d3b83
Youssef Alaa <youssefalaacj@gmail.com>
2025-01-13 01:31:37 +0200

    feat: :zap: build App Router

---
+++ lib/core/Routing/app_router.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:hola_academy/features/auth/forgot_password/UI/forgot_password_screen.dart';
+import 'package:hola_academy/features/auth/login/UI/login_screen.dart';
+import 'package:hola_academy/features/auth/reset_password/UI/reset_password.dart';
+import 'package:hola_academy/features/auth/verification/UI/verfication_screen.dart';
+
+import 'routes.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      // Login Screen 
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+
+      // forgot password
+      case Routes.forgotPassword:
+        return MaterialPageRoute(builder: (_) => const ForgotPasswordScreen());
+
+      // reset password
+      case Routes.resetPassword:
+        return MaterialPageRoute(builder: (_) => const ResetPassword());
+
+      // verification
+      case Routes.verificationScreen:
+        return MaterialPageRoute(builder: (_) => const VerificationScreen());
+
+
+      default:
+        return null;
+    }
+  }
+}
---
+++ lib/core/Routing/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const String loginScreen = "/loginScreen";
+  static const String forgotPassword = "/forgotPassword";
+  static const String verificationScreen = "/verificationScreen";
+  static const String resetPassword = "/resetPassword";
+}
