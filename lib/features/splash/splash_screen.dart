733185f703bd15930e2056872c573e2aace5a653
mayaralaa25 <100362336+mayaralaa25@users.noreply.github.com>
2025-02-05 10:04:01 +0200

    feat: :sparkles: Update AndroidManifest and add jumping_dot dependency; replace TypingIndicator with JumpingDots in splash screen

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -5,6 +5,7 @@ import 'package:hola_academy/core/constants/color_manager.dart';
 import 'dart:async';
 
 import 'package:hola_academy/core/constants/image_manager.dart';
+import 'package:jumping_dot/jumping_dot.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -24,67 +25,23 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(ImageManager.hola, width: 298.w, height: 152.h,),
+            Image.asset(
+              ImageManager.hola,
+              width: 298.w,
+              height: 152.h,
+            ),
             SizedBox(height: 20.h),
-            TypingIndicator(),
+            JumpingDots(
+              color: ColorManager.primaryOrangeColor,
+              radius: 15.r,
+              numberOfDots: 3,
+
+              animationDuration: Duration(milliseconds: 600),
+              // animationDuration = Duration(milliseconds: 200),
+            ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class TypingIndicator extends StatefulWidget {
-  const TypingIndicator({super.key});
-
-  @override
-  _TypingIndicatorState createState() => _TypingIndicatorState();
-}
-
-class _TypingIndicatorState extends State<TypingIndicator> {
-  int _currentDot = 0;
-  Timer? _timer;
-
-  @override
-  void initState() {
-    super.initState();
-    _startAnimation();
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-    super.dispose();
-  }
-
-  void _startAnimation() {
-    _timer = Timer.periodic(Duration(milliseconds: 600), (timer) {
-      setState(() {
-        _currentDot = (_currentDot + 1) % 3;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: List.generate(3, (index) {
-        return Padding(
-          padding: EdgeInsets.only(
-              left: 2.0, right: 2.0, bottom: _currentDot == index ? 12.0 : 0.0),
-          child: AnimatedContainer(
-            duration: Duration(milliseconds: 300),
-            width: 15.w,
-            height: 15.h,
-            decoration: BoxDecoration(
-              color: ColorManager.primaryOrangeColor,
-              //_currentDot == index ? Colors.grey[600] : Colors.grey[300],
-              shape: BoxShape.circle,
-            ),
-          ),
-        );
-      }),
     );
   }
 }
